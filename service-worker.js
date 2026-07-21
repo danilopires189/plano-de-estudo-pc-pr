f9fed412db546c4be4f7260233e6fd1fa299ad0c
danilopires189 <danilopires189@gmail.com>
2026-07-21 00:14:01 -0300

    fix(auth): match reference lock shape

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'plano-pc-pr-v6';
+const CACHE_NAME = 'plano-pc-pr-v7';
 const APP_SHELL = ['/', '/index.html', '/manifest.webmanifest', '/icons/icon-192.png', '/icons/icon-512.png'];
 
 self.addEventListener('install', event => {
