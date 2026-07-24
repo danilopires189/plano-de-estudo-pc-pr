20833a9e0688ae668011a4325cf684eafd6802bb
danilopires189 <danilopires189@gmail.com>
2026-07-24 11:22:14 -0300

    ajsute no filtro

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'plano-pc-pr-v12';
+const CACHE_NAME = 'plano-pc-pr-v13';
 const APP_SHELL = ['/', '/index.html', '/manifest.webmanifest', '/icons/icon-192.png', '/icons/icon-512.png'];
 
 self.addEventListener('install', event => {
