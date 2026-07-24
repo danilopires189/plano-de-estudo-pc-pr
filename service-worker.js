689c0c22f0f2644af20aec7154e4a487b4a8560a
danilopires189 <danilopires189@gmail.com>
2026-07-24 11:05:21 -0300

    ajsue visual

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'plano-pc-pr-v11';
+const CACHE_NAME = 'plano-pc-pr-v12';
 const APP_SHELL = ['/', '/index.html', '/manifest.webmanifest', '/icons/icon-192.png', '/icons/icon-512.png'];
 
 self.addEventListener('install', event => {
